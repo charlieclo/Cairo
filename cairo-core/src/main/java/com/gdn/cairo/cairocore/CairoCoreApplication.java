601964ec4a7c700c9d4eccab267136859f05eb4e
charlieclo <46519030+charlieclo@users.noreply.github.com>
2019-10-24 16:28:10 +0700

    Cairo-Core Initiate Commit

---
+++ cairo-core/src/main/java/com/gdn/cairo/cairocore/CairoCoreApplication.java
@@ -0,0 +1,13 @@
+package com.gdn.cairo.cairocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CairoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CairoCoreApplication.class, args);
+	}
+
+}
---
+++ cairo-core/src/test/java/com/gdn/cairo/cairocore/CairoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdn.cairo.cairocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CairoCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
